1d12f10c149497e08e2965daad297ad4ba0412a2
Michal Huška <michal.huska@yunextraffic.com>
2023-11-12 18:43:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const slabiky = ["ma", "Ma", "me", "me", "Le", "má", "mé", "lá", "Lé", "Lá", "ma", "me", "la", "le", "ma", "mé", "Má", "Lá", "Lé", "LE", "LA", "Sa", "sá", "sé", "SA", "sa", "se", "Sá", "Sé", "sa", "ma", "MÁ", "SE", "se", "le", "lé", "MA", "na", "La", "Sá", "La", "Sá", "SA", "SA", "ME", "LE", "KONEC"];
+let currentIndex = 0;
+
+document.getElementById("readButton").addEventListener("click", readCurrentSlabika);
+document.getElementById("nextButton").addEventListener("click", showNextSlabika);
+
+function readCurrentSlabika() {
+    let synth = window.speechSynthesis;
+    let utterance = new SpeechSynthesisUtterance(slabiky[currentIndex]);
+    utterance.lang = 'cs-CZ';
+	utterance.rate = 0.4; // Rychlost mluvení (1 je normální rychlost)
+    utterance.pitch = 1.9; // Výška hlasu (1 je normální výška)
+    synth.speak(utterance);
+}
+
+function showNextSlabika() {
+    currentIndex = (currentIndex + 1) % slabiky.length;
+    document.getElementById("slabika").textContent = slabiky[currentIndex];
+}
